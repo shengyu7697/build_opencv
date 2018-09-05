9b86ac950cbc879163c9d2c0444ef3661effc302
ShengYu <shengyu7697@gmail.com>
2018-09-05 14:54:00 +0800

    Update unix_build_3.3.1_with_contrib.sh

--- unix_build_3.3.1_with_contrib.sh
+++ unix_build_3.3.1_with_contrib.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 OPENCV_SOURCE_DIR="$1"
-cd "$OPENCV_SOURCE_DIR" && \
-git checkout 3.3.1 -b 3.3.1
-cd -
+cd "$OPENCV_SOURCE_DIR"
 
+git checkout 3.3.1 -b 3.3.1
 mkdir -p build && cd build
 cmake "$OPENCV_SOURCE_DIR" \
     -DCMAKE_INSTALL_PREFIX="/opt/opencv-3.3.1" \
